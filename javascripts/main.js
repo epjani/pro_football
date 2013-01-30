f49012493057dee7dada40df55b4e8d10b15d74b
epjani <edin.pjanic@atlantbh.com>
2013-01-30 22:07:46 +0100

    second iteration

---
+++ javascripts/main.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('.see_player').mouseover(function () {
+        $('.long_button').removeClass('long_btn').addClass('long_btn_1');
+    }).mouseout(function () {
+        $('.long_button').removeClass('long_btn_1').addClass('long_btn');
+    });
+    $('.fan_packages').mouseover(function () {
+        $('.long_button').removeClass('long_btn').addClass('long_btn_2');
+    }).mouseout(function () {
+        $('.long_button').removeClass('long_btn_2').addClass('long_btn');
+    });
+});
